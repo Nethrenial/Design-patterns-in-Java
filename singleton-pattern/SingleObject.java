354c9a77f8ff9bc526948154653689ee86605514
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-09-14 22:04:21 +0530

    fix: change protoypepattern to prototype-pattern

--- singleton-pattern/SingleObject.java
+++ singleton-pattern/SingleObject.java
@@ -1,6 +1,6 @@
 public class SingleObject {
-    private static SingleObject instance = new SingleObject();
 
+    private static SingleObject instance = new SingleObject();
     private SingleObject(){}
 
     public static SingleObject getInstance(){
