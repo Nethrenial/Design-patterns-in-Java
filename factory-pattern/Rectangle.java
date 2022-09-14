ad2ad601fc1b016b35c0d12a2212df0c7ad0dc4a
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-09-14 22:05:59 +0530

    feat: add factory pattern examples

---
+++ factory-pattern/Rectangle.java
@@ -0,0 +1,7 @@
+public class Rectangle extends Shape{
+
+    @Override
+    public void draw(){
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
---
+++ factory-pattern/Square.java
@@ -0,0 +1,6 @@
+public class Square extends Shape {
+    @Override
+    public void draw(){
+        System.out.println("Inside Square::draw() method.");
+    }
+}
