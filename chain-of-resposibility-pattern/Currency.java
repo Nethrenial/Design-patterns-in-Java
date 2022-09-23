bc8df57f13f33ac0ca850f2ce40c4f64d7877b09
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-09-23 11:03:04 +0530

    feat: add few more design patterns

---
+++ chain-of-resposibility-pattern/Currency.java
@@ -0,0 +1,11 @@
+public class Currency{
+    private int amount;
+
+    public Currency(int amt){
+        this.amount=amt;
+    }
+
+    public int getAmout(){
+        return this.amount;
+    }
+}
